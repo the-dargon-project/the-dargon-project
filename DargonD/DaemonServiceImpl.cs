b7496989ee97608e75502588c3055fec46b4a85b
Michael Yu <ItzWarty@gmail.com>
2014-10-03 01:27:01 -0700

    Move DargonD initialization (dependency injection hookups) from DaemonServiceImpl to Program.

--- DargonD/DaemonServiceImpl.cs
+++ DargonD/DaemonServiceImpl.cs
@@ -1,16 +1,5 @@
-﻿using Dargon.InjectedModule;
-using Dargon.LeagueOfLegends;
-using Dargon.ModificationRepositories;
-using Dargon.Modifications;
-using Dargon.Processes.Injection;
-using Dargon.Processes.Watching;
-using Dargon.Transport;
-using Dargon.Tray;
-using ItzWarty;
-using ItzWarty.Services;
+﻿using ItzWarty.Services;
 using NLog;
-using NLog.Config;
-using NLog.Targets;
 using System;
 using System.Threading;
 
@@ -20,76 +9,24 @@ namespace Dargon.Daemon
    {
       private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-      private readonly DaemonConfiguration configuration = new DaemonConfiguration();
-      private readonly ServiceLocator serviceLocator = new ServiceLocator();
-      private readonly IProcessProxy processProxy;
-      private readonly ProcessInjector processInjector;
-      private readonly ProcessDiscoveryMethodFactory processDiscoveryMethodFactory;
-      private readonly IProcessDiscoveryMethod processDiscoveryMethod;
-      private readonly ProcessWatcher processWatcher;
-      private readonly TrayService trayService;
-      private readonly ProcessInjectionConfiguration processInjectionConfiguration;
-      private readonly ProcessInjectionService processInjectionService;
-      private readonly ProcessWatcherService processWatcherService;
-      private readonly ModificationRepositoryService modificationRepositoryService;
-      private readonly ModificationImportService modificationImportService;
-      private readonly IDtpNodeFactory dtpNodeFactory;
-      private readonly ISessionFactory sessionFactory;
-      private readonly InjectedModuleServiceConfiguration injectedModuleServiceConfiguration;
-      private readonly InjectedModuleService injectedModuleService;
-      private readonly LeagueGameServiceImpl leagueGameServiceImpl;
+      private readonly IDaemonConfiguration configuration;
       private readonly CountdownEvent shutdownSignal = new CountdownEvent(1);
       private bool isShutdownSignalled = false;
 
       public event EventHandler BeginShutdown;
 
-      public DaemonServiceImpl()
+      public DaemonServiceImpl(IServiceLocator serviceLocator, IDaemonConfiguration configuration)
       {
-         InitializeLogging();
-
-         if (configuration.IsDebugCompilation) {
-            logger.Error("COMPILED IN DEBUG MODE");
-         }
-
          logger.Info("Initializing Daemon");
-
          serviceLocator.RegisterService(typeof(DaemonService), this);
 
-         processProxy = new ProcessProxy();
-         processInjector = new ProcessInjector();
-         processDiscoveryMethodFactory = new ProcessDiscoveryMethodFactory();
-         processDiscoveryMethod = processDiscoveryMethodFactory.CreateOptimalProcessDiscoveryMethod();
-         processWatcher = new ProcessWatcher(processProxy, processDiscoveryMethod);
-         trayService = new TrayServiceImpl(serviceLocator, this);
-         processInjectionConfiguration = new ProcessInjectionConfiguration(100, 200);
-         processInjectionService = new ProcessInjectionServiceImpl(serviceLocator, processInjector, processInjectionConfiguration);
-         processWatcherService = new ProcessWatcherServiceImpl(serviceLocator, processProxy, processWatcher).With(s => s.Initialize());
-         modificationRepositoryService = new ModificationRepositoryServiceImpl(serviceLocator);
-         modificationImportService = new ModificationImportServiceImpl(serviceLocator);
-         dtpNodeFactory = new DefaultDtpNodeFactory();
-         sessionFactory = new SessionFactory(dtpNodeFactory);
-         injectedModuleServiceConfiguration = new InjectedModuleServiceConfiguration();
-         injectedModuleService = new InjectedModuleServiceImpl(serviceLocator, processInjectionService, sessionFactory, injectedModuleServiceConfiguration);
-
-         leagueGameServiceImpl = new LeagueGameServiceImpl(this, processProxy, injectedModuleService, processWatcherService, modificationRepositoryService, modificationImportService);
+         this.configuration = configuration;
       }
 
       public IDaemonConfiguration Configuration { get { return configuration; } }
-
-      private void InitializeLogging()
-      {
-         var config = new LoggingConfiguration();
-         var debuggerTarget = new DebuggerTarget();
-         config.AddTarget("debugger", debuggerTarget);
-
-         var rule2 = new LoggingRule("*", LogLevel.Trace, debuggerTarget);
-         config.LoggingRules.Add(rule2);
-         LogManager.Configuration = config;
-      }
+      public bool IsShutdownSignalled { get { return isShutdownSignalled; } }
 
       public void Run() { shutdownSignal.Wait(); }
-
-      public bool IsShutdownSignalled { get { return isShutdownSignalled; } }
 
       public void Shutdown()
       {
--- DargonD/Program.cs
+++ DargonD/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace Dargon.Daemon
+﻿using Dargon.Game;
+using Dargon.InjectedModule;
+using Dargon.LeagueOfLegends;
+using Dargon.ModificationRepositories;
+using Dargon.Modifications;
+using Dargon.Processes.Injection;
+using Dargon.Processes.Watching;
+using Dargon.Transport;
+using Dargon.Tray;
+using ItzWarty;
+using ItzWarty.Services;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Dargon.Daemon
 {
-   class Program
+   public static class Program
    {
+      private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
       public static void Main(string[] args) { 
-         var core = new DaemonServiceImpl();
+         InitializeLogging();
+
+         var configuration = new DaemonConfiguration();
+         if (configuration.IsDebugCompilation) {
+            logger.Error("COMPILED IN DEBUG MODE");
+         }
+
+         var serviceLocator = new ServiceLocator();
+         var core = new DaemonServiceImpl(serviceLocator, configuration);
+         DaemonService daemonService = core;
+         IProcessProxy processProxy = new ProcessProxy();
+         IProcessInjector processInjector = new ProcessInjector();
+         IProcessDiscoveryMethodFactory processDiscoveryMethodFactory = new ProcessDiscoveryMethodFactory();
+         IProcessDiscoveryMethod processDiscoveryMethod = processDiscoveryMethodFactory.CreateOptimalProcessDiscoveryMethod();
+         IProcessWatcher processWatcher = new ProcessWatcher(processProxy, processDiscoveryMethod);
+         TrayService trayService = new TrayServiceImpl(serviceLocator, daemonService);
+         IProcessInjectionConfiguration processInjectionConfiguration = new ProcessInjectionConfiguration(100, 200);
+         ProcessInjectionService processInjectionService = new ProcessInjectionServiceImpl(serviceLocator, processInjector, processInjectionConfiguration);
+         ProcessWatcherService processWatcherService = new ProcessWatcherServiceImpl(serviceLocator, processProxy, processWatcher).With(s => s.Initialize());
+         ModificationRepositoryService modificationRepositoryService = new ModificationRepositoryServiceImpl(serviceLocator);
+         ModificationImportService modificationImportService = new ModificationImportServiceImpl(serviceLocator);
+         IDtpNodeFactory dtpNodeFactory = new DefaultDtpNodeFactory();
+         ISessionFactory sessionFactory = new SessionFactory(dtpNodeFactory);
+         IInjectedModuleServiceConfiguration injectedModuleServiceConfiguration = new InjectedModuleServiceConfiguration();
+         InjectedModuleService injectedModuleService = new InjectedModuleServiceImpl(serviceLocator, processInjectionService, sessionFactory, injectedModuleServiceConfiguration);
+         IGameHandler leagueGameServiceImpl = new LeagueGameServiceImpl(daemonService, processProxy, injectedModuleService, processWatcherService, modificationRepositoryService, modificationImportService);
          core.Run();
+      }
+
+      private static void InitializeLogging()
+      {
+         var config = new LoggingConfiguration();
+         var debuggerTarget = new DebuggerTarget();
+         config.AddTarget("debugger", debuggerTarget);
+
+         var rule2 = new LoggingRule("*", LogLevel.Trace, debuggerTarget);
+         config.LoggingRules.Add(rule2);
+         LogManager.Configuration = config;
       }
    }
 }

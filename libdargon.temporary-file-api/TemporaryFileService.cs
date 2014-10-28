8f50e145ffd26ff306052889e3b895eca1df577d
Michael Yu <ItzWarty@gmail.com>
2014-10-28 01:09:52 -0700

    Introduced Temporary File Api and Implementation, added libvfm reference.

---
+++ libdargon.temporary-file-api/TemporaryFileService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace Dargon
+{
+    public interface TemporaryFileService
+    {
+       IDisposable TakeLock();
+       string AllocateTemporaryDirectory(DateTime expires);
+       FileStream AllocateTemporaryFile(string temporaryDirectory, string name);
+    }
+}
---
+++ libdargon.temporary-file-impl/TemporaryFileServiceImpl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Dargon.Daemon;
+using ItzWarty;
+
+namespace Dargon
+{
+   public class TemporaryFileServiceImpl : TemporaryFileService
+   {
+      private const string EXPIRATION_FILE_NAME = ".expires";
+      private readonly IDaemonConfiguration configuration;
+      private readonly string temporaryDirectoryPath;
+      private readonly string temporaryFilesLockPath;
+      private readonly FileLock temporaryFilesLock;
+
+      public TemporaryFileServiceImpl(IDaemonConfiguration configuration) {
+         this.configuration = configuration;
+         this.temporaryDirectoryPath = configuration.TemporaryDirectoryPath;
+         this.temporaryFilesLockPath = Path.Combine(temporaryDirectoryPath, "LOCK");
+         this.temporaryFilesLock = new FileLock(temporaryFilesLockPath);
+      }
+
+      public IDisposable TakeLock() { return temporaryFilesLock.Take(); }
+      
+      public string AllocateTemporaryDirectory(DateTime expires) {
+         using (TakeLock()) {
+            var directoryPath = Path.Combine(temporaryDirectoryPath, Guid.NewGuid().ToByteArray().ToHex());
+            Util.PrepareDirectory(directoryPath);
+            File.WriteAllBytes(Path.Combine(directoryPath, EXPIRATION_FILE_NAME), BitConverter.GetBytes(expires.GetUnixTimeMilliseconds()));
+            return directoryPath;
+         }
+      }
+
+      public FileStream AllocateTemporaryFile(string temporaryDirectory, string fileName) 
+      {
+         using (TakeLock()) {
+            var path = Path.Combine(temporaryDirectory, fileName);
+            Util.PrepareParentDirectory(path);
+            return new FileStream(path, FileMode.CreateNew, FileAccess.ReadWrite, FileShare.None);
+         }
+      }
+   }
+}

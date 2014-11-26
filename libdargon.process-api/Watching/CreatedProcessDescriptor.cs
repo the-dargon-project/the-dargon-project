106666d070be732cdbb8b4bdfc15cfdd31c01f20
Michael Yu <ItzWarty@gmail.com>
2014-11-26 13:52:45 -0800

    Implement GetHashCode and Equals for CreatedProcessDescriptor.

--- libdargon.process-api/Watching/CreatedProcessDescriptor.cs
+++ libdargon.process-api/Watching/CreatedProcessDescriptor.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 
-namespace Dargon.Processes.Watching
-{
-   public class CreatedProcessDescriptor : IEquatable<CreatedProcessDescriptor>
-   {
+namespace Dargon.Processes.Watching {
+   public class CreatedProcessDescriptor : IEquatable<CreatedProcessDescriptor> {
       private readonly string processName;
       private readonly int processId;
       private readonly int parentProcessId;
 
-      public CreatedProcessDescriptor(string processName, int processId, int parentProcessId)
-      {
+      public CreatedProcessDescriptor(string processName, int processId, int parentProcessId) {
          this.processName = processName;
          this.processId = processId;
          this.parentProcessId = parentProcessId;
@@ -19,7 +16,26 @@ namespace Dargon.Processes.Watching
       public int ProcessId { get { return processId; } }
       public int ParentProcessId { get { return parentProcessId; } }
 
-      public override bool Equals(object obj) { return obj is CreatedProcessDescriptor && Equals((CreatedProcessDescriptor)obj); }
-      public bool Equals(CreatedProcessDescriptor other) { return other.processName.Equals(this.processName, StringComparison.OrdinalIgnoreCase) && other.processId == processId && other.parentProcessId == parentProcessId; }
+      public override bool Equals(object obj) {
+         if (ReferenceEquals(null, obj)) return false;
+         if (ReferenceEquals(this, obj)) return true;
+         if (obj.GetType() != this.GetType()) return false;
+         return Equals((CreatedProcessDescriptor)obj);
+      }
+
+      public bool Equals(CreatedProcessDescriptor other) {
+         if (ReferenceEquals(null, other)) return false;
+         if (ReferenceEquals(this, other)) return true;
+         return string.Equals(processName, other.processName) && processId == other.processId && parentProcessId == other.parentProcessId;
+      }
+
+      public override int GetHashCode() {
+         unchecked {
+            int hashCode = (processName != null ? processName.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ processId;
+            hashCode = (hashCode * 397) ^ parentProcessId;
+            return hashCode;
+         }
+      }
    }
 }

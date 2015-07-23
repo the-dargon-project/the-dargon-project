037f92e9132a2d4eec53a566d432be8147956383
Michael Yu <ItzWarty@gmail.com>
2015-07-23 09:50:04 -0700

    CoreDaemonEgg returns success on exit.

--- daemon-impl/CoreDaemonEgg.cs
+++ daemon-impl/CoreDaemonEgg.cs
@@ -66,7 +66,7 @@ namespace Dargon.Daemon {
          daemonService.Shutdown();
          mainThread.Join();
 
-         return NestResult.Failure;
+         return NestResult.Success;
       }
 
       private void InitializeLogging() {

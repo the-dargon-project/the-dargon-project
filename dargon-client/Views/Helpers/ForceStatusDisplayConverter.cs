a6f1ca1913db1a7b05f8a40f4eedca1145ad4954
Michael Yu <ItzWarty@gmail.com>
2015-07-31 18:56:30 -0700

    dargon-client now force-shows modification status sometimes.
    It does it when the modification is importing, has an update available,
    or is updating.

---
+++ dargon-client/Views/Helpers/ForceStatusDisplayConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Dargon.Client.ViewModels;
+
+namespace Dargon.Client.Views.Helpers {
+   public class ForceStatusDisplayConverter : IValueConverter {
+      public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+         ModificationStatus status = (ModificationStatus)value;
+         return status == ModificationStatus.Broken ||
+                status == ModificationStatus.UpdateAvailable ||
+                status == ModificationStatus.Updating;
+      }
+
+      public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+         return null;
+      }
+   }
+}
---
+++ dargon-client/Views/Helpers/LogicalOrConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Dargon.Client.Views.Helpers {
+   public class LogicalOrConverter : IMultiValueConverter {
+      public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
+         bool result = false;
+         foreach (var value in values.Cast<bool>()) {
+            result |= value;
+         }
+         return result;
+      }
+
+      public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
+         return null;
+      }
+   }
+}

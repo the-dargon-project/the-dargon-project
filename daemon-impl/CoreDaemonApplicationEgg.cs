2176f6d8dc73d02bae3eaa64f0c0f6d4a4e5a6a9
Warty <ItzWarty@gmail.com>
2015-09-14 14:23:25 -0700

    Cleanup of CoreDaemonApplicationEgg.

--- daemon-impl/CoreDaemonApplicationEgg.cs
+++ daemon-impl/CoreDaemonApplicationEgg.cs
@@ -1,50 +1,29 @@
-﻿using Castle.DynamicProxy;
-using Dargon.FinalFantasyXIII;
-using Dargon.Game;
-using Dargon.LeagueOfLegends;
-using Dargon.Management;
-using Dargon.Management.Server;
-using Dargon.Modifications;
+﻿using Dargon.Management.Server;
 using Dargon.Nest.Egg;
-using Dargon.Nest.Eggxecutor;
-using Dargon.PortableObjects;
-using Dargon.PortableObjects.Streams;
-using Dargon.Processes.Watching;
-using Dargon.Services;
-using Dargon.Services.Clustering;
-using Dargon.Transport;
-using Dargon.Tray;
-using Dargon.Trinkets.Commands;
-using Dargon.Trinkets.Spawner;
+using Dargon.Ryu;
 using ItzWarty;
-using ItzWarty.Collections;
-using ItzWarty.IO;
 using ItzWarty.Networking;
-using ItzWarty.Processes;
-using ItzWarty.Threading;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
 using NLog.Targets.Wrappers;
-using System.Collections.Generic;
-using Dargon.Ryu;
 
 namespace Dargon.Daemon {
    public class CoreDaemonApplicationEgg : INestApplicationEgg {
       private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
       private const int kDaemonManagementPort = 21001;
-      private DaemonServiceImpl daemonService;
-      private List<object> keepalive;
-      private RyuContainer ryu;
+
+      private readonly RyuContainer ryu;
+
+      public CoreDaemonApplicationEgg() {
+         ryu = new RyuFactory().Create();
+         ((RyuContainerImpl)ryu).SetLoggerEnabled(true);
+      }
 
       public NestResult Start(IEggParameters parameters) {
-         if (!logger.IsErrorEnabled) {
-            InitializeLogging();
-         }
-
+         InitializeLogging();
          LogIfDebugBuild();
-
-         ryu = new RyuFactory().Create();
 
          ryu.Set<IEggHost>(parameters?.Host);
 
@@ -55,7 +34,6 @@ namespace Dargon.Daemon {
          var managementServerEndpoint = ryu.Get<INetworkingProxy>().CreateAnyEndPoint(kDaemonManagementPort);
          ryu.Set<IManagementServerConfiguration>(new ManagementServerConfiguration(managementServerEndpoint));
 
-         ((RyuContainerImpl)ryu).SetLoggerEnabled(true);
          ((RyuContainerImpl)ryu).Setup(true);
          
          logger.Info("Initialized.");
@@ -64,14 +42,8 @@ namespace Dargon.Daemon {
       }
 
       public NestResult Shutdown() {
-         daemonService.Shutdown();
+         ryu.Get<DaemonService>().Shutdown();
          return NestResult.Success;
-      }
-
-      private void LogIfDebugBuild() {
-#if DEBUG
-         logger.Error("COMPILED IN DEBUG MODE");
-#endif
       }
 
       private void InitializeLogging() {
@@ -87,7 +59,7 @@ namespace Dargon.Daemon {
          debuggerTarget = new AsyncTargetWrapper(debuggerTarget);
          consoleTarget = new AsyncTargetWrapper(consoleTarget);
 #else
-         AsyncTargetWrapper a; // Placeholder for optimizing imports
+         new AsyncTargetWrapper().Wrap(); // Placeholder for optimizing imports
 #endif
 
          config.AddTarget("debugger", debuggerTarget);
@@ -102,124 +74,10 @@ namespace Dargon.Daemon {
          LogManager.Configuration = config;
       }
 
-      private static DaemonServiceImpl CreateDaemonCore(IEggParameters parameters, out List<object> keepalive) {
-         keepalive = new List<object>();
-         keepalive.Add(parameters);
-
-         // construct libwarty dependencies
-         ICollectionFactory collectionFactory = new CollectionFactory();
-
-         // construct libwarty-proxies dependencies
-         IStreamFactory streamFactory = new StreamFactory();
-         IFileSystemProxy fileSystemProxy = new FileSystemProxy(streamFactory);
-         IThreadingFactory threadingFactory = new ThreadingFactory();
-         ISynchronizationFactory synchronizationFactory = new SynchronizationFactory();
-         IThreadingProxy threadingProxy = new ThreadingProxy(threadingFactory, synchronizationFactory);
-         IDnsProxy dnsProxy = new DnsProxy();
-         ITcpEndPointFactory tcpEndPointFactory = new TcpEndPointFactory(dnsProxy);
-         INetworkingInternalFactory networkingInternalFactory = new NetworkingInternalFactory(threadingProxy, streamFactory);
-         ISocketFactory socketFactory = new SocketFactory(tcpEndPointFactory, networkingInternalFactory);
-         INetworkingProxy networkingProxy = new NetworkingProxy(socketFactory, tcpEndPointFactory);
-         IProcessProxy processProxy = new ProcessProxy();
-
-         // construct Castle.Core dependencies
-         ProxyGenerator proxyGenerator = new ProxyGenerator();
-
-         // construct dargon common Portable Object Format dependencies
-         IPofContext pofContext = new ClientPofContext();
-         IPofSerializer pofSerializer = new PofSerializer(pofContext);
-
-         // construct libdargon.management dependencies
-         ITcpEndPoint managementServerEndpoint = networkingProxy.CreateAnyEndPoint(kDaemonManagementPort);
-         var managementFactory = new ManagementFactoryImpl(collectionFactory, threadingProxy, networkingProxy, pofContext, pofSerializer);
-         var localManagementServer = managementFactory.CreateServer(new ManagementServerConfiguration(managementServerEndpoint));
-         keepalive.Add(localManagementServer);
-
-         // construct root Dargon dependencies.
-         var clientConfiguration = new ClientConfiguration();
-
-         // construct system-state dependencies
-         var systemState = new ClientSystemStateFactory(fileSystemProxy, clientConfiguration).Create();
-         localManagementServer.RegisterInstance(new ClientSystemStateMob(systemState));
-
-         // construct libdsp dependencies
-         PofStreamsFactory pofStreamsFactory = new PofStreamsFactoryImpl(threadingProxy, streamFactory, pofSerializer);
-         ServiceClientFactoryImpl serviceClientFactory = new ServiceClientFactoryImpl(proxyGenerator, streamFactory, collectionFactory, threadingProxy, networkingProxy, pofSerializer, pofStreamsFactory);
-
-         // construct libdsp local service node
-         ClusteringConfiguration clusteringConfiguration = new ClientClusteringConfiguration();
-         ServiceClient localServiceClient = serviceClientFactory.Construct(clusteringConfiguration);
-         keepalive.Add(localServiceClient);
-
-         // construct Dargon Daemon dependencies
-         var core = new DaemonServiceImpl(parameters.Host, clientConfiguration);
-         DaemonService daemonService = core;
-         localServiceClient.RegisterService(daemonService, typeof(DaemonService));
-         localManagementServer.RegisterInstance(new DaemonServiceMob(core));
-
-         // construct miscellanious common Dargon dependencies
-         TemporaryFileService temporaryFileService = new TemporaryFileServiceImpl(clientConfiguration, fileSystemProxy).With(x => x.Initialize());
-         localServiceClient.RegisterService(temporaryFileService, typeof(TemporaryFileService));
-         IDtpNodeFactory dtpNodeFactory = new DefaultDtpNodeFactory();
-
-         // get the exeggutor service
-         ExeggutorService exeggutorService = localServiceClient.GetService<ExeggutorService>();
-
-         // construct modification and repository dependencies
-         var modificationComponentFactory = new ModificationComponentFactory(fileSystemProxy, pofContext, new SlotSourceFactoryImpl(), pofSerializer);
-         ModificationLoader modificationLoader = new ModificationLoaderImpl(clientConfiguration, modificationComponentFactory);
-
-         // construct process watching/injection dependencies
-//         IProcessInjector processInjector = new ProcessInjector();
-         IProcessDiscoveryMethodFactory processDiscoveryMethodFactory = new ProcessDiscoveryMethodFactory();
-         IProcessDiscoveryMethod processDiscoveryMethod = processDiscoveryMethodFactory.CreateOptimalProcessDiscoveryMethod();
-         IProcessWatcher processWatcher = new ProcessWatcher(processProxy, processDiscoveryMethod);
-         TrayService trayService = new TrayServiceImpl(daemonService);
-//         IProcessInjectionConfiguration processInjectionConfiguration = new ProcessInjectionConfiguration(100, 200);
-//         ProcessInjectionService processInjectionService = new ProcessInjectionServiceImpl(processInjector, processInjectionConfiguration);
-         ProcessWatcherService processWatcherService = new ProcessWatcherServiceImpl(processProxy, processWatcher).With(s => s.Initialize());
-         //         ISessionFactory sessionFactory = new SessionFactory(dtpNodeFactory);
-         //         IInjectedModuleServiceConfiguration injectedModuleServiceConfiguration = new InjectedModuleServiceConfiguration();
-         //         InjectedModuleService injectedModuleService = new InjectedModuleServiceImpl(processInjectionService, sessionFactory, injectedModuleServiceConfiguration).With(x => x.Initialize());
-         //         localServiceClient.RegisterService(injectedModuleService, typeof(InjectedModuleService));
-         //         localServiceClient.RegisterService(processInjectionService, typeof(ProcessInjectionService));
-
-         CommandFactory commandFactory = new CommandFactoryImpl();
-         TrinketSpawner trinketSpawner = new TrinketSpawnerImpl(streamFactory, pofSerializer, exeggutorService);
-
-//         var sectors = new SectorCollection();
-//         sectors.AssignSector(new SectorRange(0, 26), new FileSector("C:/dummy-files/lowercase.txt", 0, 26));
-//         sectors.AssignSector(new SectorRange(28, 54), new FileSector("C:/dummy-files/uppercase.txt", 0, 26));
-//         sectors.AssignSector(new SectorRange(54, 64), new FileSector("C:/dummy-files/digits.txt", 0, 10));
-//         sectors.AssignSector(new SectorRange(65, 68), new FileSector("C:/dummy-files/digits.txt", 3, 6));
-//         var vfm = new VirtualFile(sectors);
-//         var vfmSerializer = new SectorCollectionSerializer();
-//         using (var fs = fileSystemProxy.OpenFile("C:/dummy-files/z.vfm", FileMode.Create, FileAccess.Write))
-//         using (var writer = fs.Writer) {
-//            vfmSerializer.Serialize(sectors, writer.__Writer);
-//         }
-//
-//         var targetProcess = processWatcher.FindProcess(x => x.ProcessName.Contains("notepad++")).First();
-//         logger.Info("TARGET PID " + targetProcess.Id);
-//         trinketSpawner.SpawnTrinket(
-//            targetProcess, 
-//            new TrinketSpawnConfigurationImpl {
-//               Name = "npp",
-//               IsFileSystemOverridingEnabled = true,
-//               IsFileSystemHookingEnabled = true,
-//               IsCommandingEnabled = true,
-//               CommandList = new DefaultCommandList(new [] {
-//                  commandFactory.CreateFileRemappingCommand("C:/dummy-files/a.txt", "C:/Users/ItzWarty/.dargon/temp/9b2e6d0cbf57024d96ff646476398f26/0.0.0.235/Archive_3.raf.dat.vfm")
-//               })
-//            }
-//         );
-//         logger.Info("######################################");
-
-         // construct additional Dargon dependencies
-//         IGameHandler leagueGameServiceImpl = new LeagueGameServiceImpl(clientConfiguration, threadingProxy, fileSystemProxy, systemState, localManagementServer, daemonService, temporaryFileService, processProxy, processWatcherService, modificationLoader, trinketSpawner);
-//         IGameHandler ffxiiiGameServiceImpl = new FFXIIIGameServiceImpl(daemonService, processProxy, processWatcherService);
-
-         return core;
+      private void LogIfDebugBuild() {
+#if DEBUG
+         logger.Error("COMPILED IN DEBUG MODE");
+#endif
       }
    }
 }
